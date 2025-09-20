18352b8f13674dfe74ef30f5daa7014aac8c8770
Keinazj <jafari.keinaz@gmail.com>
2025-09-20 13:28:56 +0330

    backend refactor 2

--- api/middleware.py
+++
@@ -1,9 +0,0 @@
-from django.utils.deprecation import MiddlewareMixin
-from django.contrib.auth.models import AnonymousUser
-from api.auth import MemberBackend
-
-class MemberSessionMiddleware(MiddlewareMixin):
-    def process_request(self, request):
-        member_id = request.session.get("member_id")
-        user_obj  = MemberBackend().get_user(member_id)
-        request.user = user_obj if user_obj else AnonymousUser()
